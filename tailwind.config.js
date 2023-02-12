28a62438882f96f3656bcd4ebe4582174446f349
Marek Vcelak <vcelak.marek@gmail.com>
2023-02-12 15:15:17 +0100

    add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple": "var(--purple)",
+        "purple-hover": "var(--purple-hover)",
+        "red":"var(--red)",
+        "red-hover":"var(--red-hover)",
+        "dark-blue":"var(--dark-blue)",
+        "blue":"var(--blue)",
+        "light-blue":"var(--light-blue)",
+        "light-grey":"var(--light-grey)",
+        "grey":"var(--grey)",
+        "black":"var(--black)",
+        "black2":"var(--black2)",
+        "light-bg":"var(--light-bg)"
+      },
+    },
   },
   plugins: [],
 };
