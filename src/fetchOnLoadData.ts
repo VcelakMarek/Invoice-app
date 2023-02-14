bde6ec607479da25ddb6477b915eff1af78edfd8
Marek Vcelak <vcelak.marek@gmail.com>
2023-02-14 21:35:01 +0100

    change type of setInvoices

--- src/fetchOnLoadData.ts
+++ src/fetchOnLoadData.ts
@@ -1,7 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 
 export const fetchOnLoadData = async (
-  setInvoices: Dispatch<SetStateAction<unknown>>
+  setInvoices: Dispatch<SetStateAction<unknown[]>>
 ) => {
   const res = await fetch(`/Invoice_app/onLoadData.json`);
 
