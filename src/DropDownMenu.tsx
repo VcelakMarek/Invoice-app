a4f197531dc33f35035be731a27589bcb19be74a
Marek Vcelak <vcelak.marek@gmail.com>
2023-02-14 21:28:22 +0100

    add DropDownMenu.tsx

---
+++ src/DropDownMenu.tsx
@@ -0,0 +1,11 @@
+const DropDownMenu = () => {
+  return (
+    <>
+      <input type="checkbox">Draft</input>
+      <input type="checkbox">Pending</input>
+      <input type="checkbox">Paid</input>
+    </>
+  );
+};
+
+export default DropDownMenu;
