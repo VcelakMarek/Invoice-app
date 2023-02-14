55725ef64324c6197dd244be6a7e29fd2a7c1cdd
Marek Vcelak <vcelak.marek@gmail.com>
2023-02-14 12:44:10 +0100

    add fetchOnLoadData.ts

---
+++ src/fetchOnLoadData.ts
@@ -0,0 +1,10 @@
+import { Dispatch, SetStateAction } from "react";
+
+export const fetchOnLoadData = async (
+  setInvoices: Dispatch<SetStateAction<unknown>>
+) => {
+  const res = await fetch(`/Invoice_app/onLoadData.json`);
+
+  const data = await res.json();
+  setInvoices(data);
+};
