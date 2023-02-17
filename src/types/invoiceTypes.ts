cfa90118fc34eec369efcfd8f7f49977179c9ed2
Marek Včelák <vcelak.marek@gmail.com>
2023-02-17 19:44:10 +0100

    add types folder

---
+++ src/types/invoiceTypes.ts
@@ -0,0 +1,28 @@
+export interface Address {
+  street: string;
+  city: string;
+  postCode: string;
+  country: string;
+}
+
+export interface Item {
+  name: string;
+  quantity: number;
+  price: number;
+  total: number;
+}
+
+export interface InvoiceTypes {
+  id: string;
+  createdAt?: string;
+  paymentDue: string;
+  description?: string;
+  paymentTerms?: number;
+  clientName: string;
+  clientEmail?: string;
+  status: string;
+  senderAddress?: Address;
+  clientAddress?: Address;
+  items?: Item[];
+  total: number;
+}
