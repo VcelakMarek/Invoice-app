289cfdf859dd44381dff6e1a03ed8d61b658b061
Marek Včelák <vcelak.marek@gmail.com>
2023-02-17 19:56:57 +0100

    wip

--- src/DropDownMenu.tsx
+++ src/DropDownMenu.tsx
@@ -1,10 +1,28 @@
-const DropDownMenu = () => {
+const DropDownMenu = (options: any) => {
   return (
-    <>
-      <input type="checkbox">Draft</input>
-      <input type="checkbox">Pending</input>
-      <input type="checkbox">Paid</input>
-    </>
+    <div className="absolute right-[340px] top-28 h-32 w-48 rounded-lg bg-[#FFF]">
+      {options.map((option) => (
+        <div key={option}>
+          <input id={option} type="checkbox"></input>
+          <label htmlFor={option}>{option}</label>
+        </div>
+      ))}
+    </div>
+
+    // <div className="absolute right-[340px] top-28 h-32 w-48 rounded-lg bg-[#FFF]">
+    //   <div>
+    //     <input id="draft" type="checkbox"></input>
+    //     <label htmlFor="draft">Draft</label>
+    //   </div>
+    //   <div>
+    //     <input id="pending" type="checkbox"></input>
+    //     <label htmlFor="pending">Pending</label>
+    //   </div>
+    //   <div>
+    //     <input id="paid" type="checkbox"></input>
+    //     <label htmlFor="paid">Paid</label>
+    //   </div>
+    // </div>
   );
 };
 
