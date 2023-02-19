dc6720da37fa35dbea358cd47194b8202683bc80
Marek Vcelak <vcelak.marek@gmail.com>
2023-02-19 14:47:37 +0100

    add react router, import Details.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InvoiceApp from "./InvoiceApp";
+import Details from "./Details";
 
 const App = () => {
-  return <InvoiceApp />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Invoice_app/" element={<InvoiceApp />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 const container = document.getElementById("root");
