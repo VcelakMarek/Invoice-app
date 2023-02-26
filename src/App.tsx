f2c89d0f1cf8168ea867b0aaaca6a5aa7ea0f0aa
Marek Včelák <vcelak.marek@gmail.com>
2023-02-26 09:42:32 +0100

    fix z-indexes

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="h-screen bg-light-bg">
-        <nav className="fixed flex h-screen w-[103px] flex-col justify-between rounded-tr-[20px] rounded-br-[20px] bg-blue">
-          <div className="relative z-10 h-[103px] w-[103px] rounded-tr-[20px] rounded-br-[20px] bg-purple">
+        <nav className="fixed z-20 flex h-screen w-[103px] flex-col justify-between rounded-tr-[20px] rounded-br-[20px] bg-blue">
+          <div className="relative z-20 h-[103px] w-[103px] rounded-tr-[20px] rounded-br-[20px] bg-purple">
             <img
-              className="absolute top-8 left-8 z-30 w-10"
+              className="absolute top-8 left-8 z-40 w-10"
               src="/Invoice_app/assets/logo.svg"
               alt="logo"
             />
-            <div className="absolute top-1/2 z-20 h-[51px] w-full rounded-tl-[20px] rounded-br-[20px] bg-purple-hover"></div>
+            <div className="absolute top-1/2 z-30 h-[51px] w-full rounded-tl-[20px] rounded-br-[20px] bg-purple-hover"></div>
           </div>
 
           <div className="grid h-[167px] place-items-center">
