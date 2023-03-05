af498c0384c9f0e25820641382839333767dd883
Marek Vcelak <vcelak.marek@gmail.com>
2023-03-05 16:44:05 +0100

    add select inputType

--- src/FormInput.tsx
+++ src/FormInput.tsx
@@ -1,7 +1,9 @@
 type Props = {
-  size?: string;
+  size?: "small" | "medium" | "large";
   inputName: string;
   customId?: string;
+  inputType?: "text" | "date" | "select";
+  selectValues?: string[];
   // inputSize: { [key: string]: string };
 };
 
@@ -11,17 +13,36 @@ const inputSize: { [key: string]: string } = {
   large: "w-[504px]",
 };
 
-const FormInput = ({ size = "large", inputName, customId }: Props) => {
+const FormInput = ({
+  size = "large",
+  inputName,
+  customId,
+  inputType = "text",
+  selectValues,
+}: Props) => {
   const id = customId ? customId : inputName.replace(" ", "");
 
-  console.log("id", id);
-
-  return (
-    <label htmlFor={id}>
-      <h2>{inputName}</h2>
-      <input className={inputSize[size]} type="text" id={id} />
-    </label>
-  );
+  if (inputType === "select") {
+    return (
+      <label htmlFor={id}>
+        <h2>{inputName}</h2>
+        <select className={inputSize[size]} id={id}>
+          {selectValues?.map((value) => (
+            <option value={value} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </label>
+    );
+  } else {
+    return (
+      <label htmlFor={id}>
+        <h2>{inputName}</h2>
+        <input className={inputSize[size]} type={inputType} id={id} />
+      </label>
+    );
+  }
 };
 
 export default FormInput;
