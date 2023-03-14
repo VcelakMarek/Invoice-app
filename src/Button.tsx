d4c03f7b41023372e1691f065c129b1c825736ca
Marek Vcelak <vcelak.marek@gmail.com>
2023-03-14 17:56:12 +0100

    add transparent variant, icon button, fix onClick

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,12 +2,13 @@ import { ReactNode, useState } from "react";
 import DropDownMenu from "./DropDownMenu";
 
 type Props = {
-  color?: "red" | "purple" | "grey" | "darkBlue";
+  color?: "red" | "purple" | "grey" | "darkBlue" | "transparent";
   children?: ReactNode;
   dropDown?: boolean;
-  onCLick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   invoice?: boolean;
   DropDownMenu?: ReactNode;
+  icon?: boolean;
 };
 
 const backgroundColor = {
@@ -15,6 +16,7 @@ const backgroundColor = {
   purple: "bg-purple hover:bg-purple-hover",
   grey: "bg-[#e6e8f5] hover:bg-light-grey",
   darkBlue: "bg-blue hover:bg-black",
+  transparent: "",
 };
 
 const textColor = {
@@ -22,19 +24,23 @@ const textColor = {
   purple: "text-white",
   grey: "text-light-blue",
   darkBlue: "text-grey",
+  transparent: "",
 };
 
 const Button = ({
-  color = "purple",
+  color = "transparent",
   dropDown,
-  onCLick,
+  onClick,
   invoice,
   children,
+  icon,
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const border = !dropDown ? "rounded-full" : null;
-  const dimensions = `h-12 ${invoice ? "pl-2" : "pl-6"} pr-6`;
+  const dimensions = !icon
+    ? `h-12 ${invoice ? "pl-2" : "pl-6"} pr-6`
+    : " pl-5 pb-2";
   const text = "font-bold text-xs tracking-[1px]";
   const flex = "flex items-center gap-4";
 
@@ -72,7 +78,7 @@ const Button = ({
     );
   } else if (invoice) {
     return (
-      <button className={invoiceClasses.join(" ")} onClick={onCLick}>
+      <button className={invoiceClasses.join(" ")} onClick={onClick}>
         <div className="grid h-8 w-8 place-items-center rounded-full bg-white">
           <img
             className="pl-px pt-px"
@@ -85,13 +91,7 @@ const Button = ({
     );
   } else {
     return (
-      <button
-        className={baseClasses.join(" ")}
-        onClick={(e) => {
-          e.preventDefault();
-          onclick;
-        }}
-      >
+      <button type="button" className={baseClasses.join(" ")} onClick={onClick}>
         {children}
       </button>
     );
