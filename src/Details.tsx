23771ab06b2bc2c727a0cf8768930da015f8fd08
Marek Vcelak <vcelak.marek@gmail.com>
2023-03-27 17:50:19 +0200

    add delete button, markAsPaid button

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,11 +1,35 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { InvoicesContext } from "./Invoices.context";
 import Button from "./Button";
 import Invoice from "./Invoice";
 import Status from "./Status";
+import { InvoiceTypes } from "./types/invoiceTypes";
 
 const Details = () => {
   const location = useLocation();
   const { invoiceData } = location.state;
+  const navigate = useNavigate();
+  const [invoices, setInvoices] = useContext(InvoicesContext);
+
+  const deleteInvoice = () => {
+    setInvoices((current: InvoiceTypes[]) =>
+      current.filter((invoice) => invoice.id !== invoiceData.id)
+    );
+  };
+
+  const markAsPaid = () => {
+    setInvoices(
+      invoices.map((obj) => {
+        if (obj.id === invoiceData.id) {
+          invoiceData.status = "paid";
+          return { ...obj, status: "paid" };
+        } else {
+          return obj;
+        }
+      })
+    );
+  };
 
   return (
     <div className="m-auto w-[730px] pt-8">
@@ -17,8 +41,24 @@ const Details = () => {
         </div>
         <div className="flex gap-2">
           <Button color="grey">Edit</Button>
-          <Button color="red">Delete</Button>
-          <Button color="purple">Mark as Paid</Button>
+          <Button
+            color="red"
+            onClick={() => {
+              deleteInvoice();
+              navigate("/Invoice_app/");
+            }}
+          >
+            Delete
+          </Button>
+          <Button
+            color="purple"
+            onClick={() => {
+              markAsPaid();
+            }}
+            disabled={invoiceData.status === "paid"}
+          >
+            Mark as Paid
+          </Button>
         </div>
       </div>
       <Invoice
