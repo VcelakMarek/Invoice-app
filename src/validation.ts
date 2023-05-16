d0a385eaeb47d08d599b1f11ce841b7b04a6198d
VcelakMarek <vcelak.marek@gmail.com>
2023-05-16 17:15:34 +0200

    change validationSchema

--- src/validation.ts
+++ src/validation.ts
@@ -5,35 +5,24 @@ import { AnyObject } from "final-form";
 type ValidationSchema = Yup.Schema<AnyObject>;
 
 const validationSchema = Yup.object().shape({
-  id: Yup.string(),
-  createdAt: Yup.string().required(""),
-  paymentDue: Yup.string().required(""),
-  description: Yup.string().required(""),
-  paymentTerms: Yup.number().required(""),
-  clientName: Yup.string().required(""),
-  clientEmail: Yup.string().required(""),
-  status: Yup.string(),
-  senderAddress: Yup.object({
-    street: Yup.string().required(""),
-    city: Yup.string().required(""),
-    postCode: Yup.string().required(""),
-    country: Yup.string().required(""),
-  }),
-  clientAddress: Yup.object({
-    street: Yup.string().required(""),
-    city: Yup.string().required(""),
-    postCode: Yup.string().required(""),
-    country: Yup.string().required(""),
-  }),
-  items: Yup.array().of(
-    Yup.object({
-      name: Yup.string().required(""),
-      quantity: Yup.number().required(""),
-      price: Yup.number().required(""),
-      total: Yup.number(),
-    })
-  ),
-  total: Yup.number(),
+  fromStreetAddress: Yup.string().required("Street Address is required"),
+  fromCity: Yup.string().required("City is required"),
+  fromPostCode: Yup.string().required("Post Code is required"),
+  fromCountry: Yup.string().required("Country is required"),
+  toClientsName: Yup.string().required("Client's Name is required"),
+  toClientsEmail: Yup.string()
+    .email("Invalid email")
+    .required("Client's Email is required"),
+  toStreetAddress: Yup.string().required("Street Address is required"),
+  toCity: Yup.string().required("City is required"),
+  toPostCode: Yup.string().required("Post Code is required"),
+  toCountry: Yup.string().required("Country is required"),
+  createdAt: Yup.date().required("Issue Date is required"),
+  paymentTerms: Yup.string().required("Payment Terms is required"),
+  description: Yup.string().required("Project Description is required"),
+  "items[].name": Yup.string().required("Item Name is required"),
+  "items[].quantity": Yup.number().required("Quantity is required"),
+  "items[].price": Yup.number().required("Price is required"),
 });
 
 const validateSchemaAsync = (schema: ValidationSchema) => (values: any) => {
@@ -51,7 +40,7 @@ const validateSchemaAsync = (schema: ValidationSchema) => (values: any) => {
             setIn(formError, innerError.path as string, innerError.message),
           {}
         );
-        console.log("Píčo error", errors);
+        console.log("error", errors);
         return Promise.resolve(errors);
       });
   }
